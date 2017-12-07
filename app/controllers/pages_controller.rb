473b0dbd969dfe95fc45403932662956c313109d
Rebecca Stone <ysbecca@gmail.com>
2017-12-07 16:53:02 +0000

    basic clearance authentication for users

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+#apps/controllers/pages_controller.rb
+ 
+class PagesController < ApplicationController
+  before_action :require_login, only: [:new]
+   
+  def index
+  end
+ 
+  def new
+  end
+end
---
+++ config/initializers/clearance.rb
@@ -0,0 +1,18 @@
+#config/initializers/clearance.rb
+ 
+Clearance.configure do |config|
+  config.routes = false
+  config.allow_sign_up = true
+  # config.cookie_domain = ".example.com"
+  # config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  # config.cookie_name = "remember_token"
+  # config.cookie_path = "/"
+  # config.routes = true
+  # config.httponly = false
+  config.mailer_sender = "ysbecca@gmail.com"
+  config.password_strategy = Clearance::PasswordStrategies::BCrypt
+  config.redirect_url = "/"
+  config.secure_cookie = false
+  config.sign_in_guards = []
+  config.user_model = User
+end
